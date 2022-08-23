3f77237d2001c78979f271ab3ba95d659a207351
222e026 <hotaluna238dia@gmail.com>
2022-08-23 16:11:44 +0900

    program

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
 ﻿using InputUtilitys;
-namespace Ex_2022_Template
+namespace Ex_26_02_iida
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var name = InputUtility.Input("Name:");
-            var age = InputUtility.InputNumber("Age:", 120);
-            Console.WriteLine($"Hello {name}! \n U are {age} yeaes old.");
+            var width = InputUtility.InputFloat("幅を入力してください");
+            var height = InputUtility.InputFloat("高さを入力してください");
+            var depth = InputUtility.InputFloat("奥行を入力してください");
+            Box box = new Box(width, height, depth);
+
+            Console.WriteLine("表面積 = "+box.GetSurface());
+            Console.WriteLine("体積 = "+box.GetVolume());
+        }
+    }
+    class Box
+    {
+        float width,height,depth;
+        public Box(float width,float height,float depth)
+        {
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
+        }
+        public float GetSurface()
+        {
+            return (width*height+width*depth+height*depth)*2;
+        }
+        public float GetVolume()
+        {
+            return width*height*depth;
         }
     }
 }
